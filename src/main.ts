b72a3e621f86efe1e95ec4bf3f50db51272aada2
Corbin Crutchley <git@crutchcorn.dev>
2023-06-04 05:39:47 -0700

    Migrate to entirely different syntax

--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,72 @@
-type Comp = string | ((args: any) => HTMLElement);
+function createState<T>(initialValue: T): {listeners: Array<() => void>, value: T} {
+  let val = initialValue;
+  let listeners = [] as Array<() => void>;
+  return {
+    get value() {
+      return val;
+    },
+    set value(v: T) {
+      val = v;
+      listeners.forEach(fn => fn());
+    },
+    listeners
+  };
+}
 
-function c(tagOrComp: Comp, props: object, children: Array<Comp>) {
-  if (typeof tagOrComp === "function" ) {
-    return tagOrComp({...props, children});
+const elements = {
+  App(rootEl) {
+    let count = createState(0);
+
+    function updateCount() {
+      count.value++;
+    }
+
+    return {
+      count,
+      updateCount
+    }
   }
-  const el = document.createElement(tagOrComp);
-  for (let _prop of Object.keys(props)) {
-    const prop = _prop as keyof typeof props;
-    if (_prop.startsWith("on")) {
-      const name = _prop.replace(/^on([A-Z])/, (match) => match[2].toLowerCase())
-      el.addEventListener(name, props[prop]);
+} as Record<string, (el: HTMLElement) => Record<string, unknown>>
+
+function bindAndHandleElement<T extends Record<string, unknown>>(el: HTMLElement, data: T) {
+  const dataKeys =Object.keys(el.dataset);
+  for (let key of dataKeys) {
+    if (key.startsWith("on")) {
+      const name = key.replace(/^on([A-Z])/, (match) => match[2].toLowerCase())
+      let fnNameWithCall = el.dataset[key]!;
+      // TODO: Parse props being passed, bind them to the `data` keyword
+      if (fnNameWithCall.endsWith("()")) {
+        fnNameWithCall = fnNameWithCall.slice(0, fnNameWithCall.length - 2);
+      }
+      el.addEventListener(name, data[fnNameWithCall as never] as () => void);
       continue;
     }
-    if (prop in el) {
-      el[prop] = props[prop] as never;
-      continue;
-    }
-
-    el.setAttribute(prop, props[prop]);
-  }
-  for (let child of children) {
-    if (typeof child === "string") {
-      el.innerText = child;
-    } else {
-      const childEl = c(child, {}, []);
-      el.append(childEl);
+    if (key === "bind") {
+      const mutableState = data[el.dataset[key] as never] as ReturnType<typeof createState>
+      mutableState.listeners.push(() => {
+        el.innerText = mutableState.value as string;
+      });
+      el.innerText = mutableState.value as string;
     }
   }
-  return el;
 }
 
-function Child() {
-  return c("button", {onClick: () => alert("Hi"), test: 123}, [
-    "Test"
-  ]);
+function render(compName: string, rootEl: HTMLElement) {
+  const data = elements[compName]?.(rootEl);
+  // Roots cannot bind anything
+  function bindAndHandleChildren(children: HTMLElement[]) {
+    for (let child of children) {
+      bindAndHandleElement(child, data);
+      if (child.children) {
+        bindAndHandleChildren([...child.children] as HTMLElement[])
+      }
+    }
+  }
+  bindAndHandleChildren([...rootEl.children] as HTMLElement[]);
 }
 
-function App() {
-  return c("div", {}, [
-    Child
-  ])
+const roots = [...document.querySelectorAll("[data-island-comp]")] as HTMLElement[];
+
+for (let root of roots) {
+  render(root.dataset.islandComp!, root);
 }
-
-function render(el: HTMLElement, Comp: Comp) {
-  el.append(c(Comp, {}, [])!);
-}
-
-
-render(document.querySelector<HTMLElement>('#app')!, App)
